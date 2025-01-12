3650aa23879b23150650ad9dd698c4a957158f13
SeniorSam9 <rxrld050@hotmail.com>
2025-01-12 21:20:36 +0300

    '

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
+// libs
 const express = require("express");
 const server = express();
 const port = 3000;
+const cors = require("cors");
+const letterRoute = require("./src/routes/lettersRoute");
 
-server.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+// configs
+server.use(cors());
+server.use(express.json());
+
+//middleware
+server.use("/letter", letterRoute);
 
 server.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
--- src/routes/lettersRoute.js
+++ src/routes/lettersRoute.js
@@ -1,2 +1,7 @@
 const express = require("express");
 const router = express.Router();
+
+router.post("/add-letter", (req, res) => {
+  console.log(req.body);
+});
+module.exports = router;
